7cb995a3ecc0d2bb9f9976ce5eafe76dddae8e97
cookiesjuice <34698015+cookiesjuice@users.noreply.github.com>
2020-08-16 22:13:17 +0800

    Removed serveral contacts

--- src/components/info/pages/Contact.vue
+++ src/components/info/pages/Contact.vue
@@ -3,9 +3,11 @@
     <Title title="联系方式"/>
     <ContactEntry img="github.png" text="github.com/cookiesjuice" href="https://github.com/cookiesjuice/"/>
     <ContactEntry img="email.png" text="hy140866@gmail.com" href="mailto:hy140866@gmail.com"/>
+    <!--
     <ContactEntry img="twitter.png" text="@Cookiesjuice" href="https://twitter.com/Cookiesjuice"/>
     <ContactEntry img="steam.png" text="Steam" href="https://steamcommunity.com/id/cookiesjuice/"/>
     <ContactEntry img="qq.png" text="570185461" href="assets/qq_qrcode.png"/>
+    -->
     <ContactEntry img="wechat.png" text="cookiesjuice" href="assets/wechat_qrcode.png"/>
   </div>
 </template>
